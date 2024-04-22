ddbaa2f677b3dbdae0a700d44000206839d6e67a
AnhTuan1407 <anhtuan2003147@gmail.com>
2024-04-23 00:23:31 +0700

    update

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  mode: 'jit',
+  purge: [
+    './**/*.html',
+    './**/*.{js,jsx,ts,tsx,vue}',
+  ],
+  content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        35: '35px',
+      }
+    },
   },
   plugins: [],
 }
